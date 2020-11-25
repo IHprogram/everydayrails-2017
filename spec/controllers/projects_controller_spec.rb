d4fccfa9ef2b5c57faf40cab478b34b6e3a3e985
IHprogram <ihm20202020@gmail.com>
2020-11-25 10:46:49 +0900

    updateアクションに関するテスト

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -66,17 +66,16 @@ RSpec.describe ProjectsController, type: :controller do
   describe "#create" do
     # 認証済みのユーザーとして
     context "as an authenticated user" do
-      before do
-        let(:user){ FactoryBot.create(:user) }
-      end
+      let(:user){ FactoryBot.create(:user) }
+
 
       # プロジェクトを追加できること
       it "adds a project" do
         project_params = FactoryBot.attributes_for(:project)
-        sign_in @user
+        sign_in user
         expect {
           post :create, params: { project: project_params }
-        }.to change(@user.projects, :count).by(1)
+        }.to change(user.projects, :count).by(1)
       end
     end
 
@@ -93,6 +92,48 @@ RSpec.describe ProjectsController, type: :controller do
         project_params = FactoryBot.attributes_for(:project)
         post :create, params: { project: project_params }
         expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
+  describe "#update" do
+    #認可されたユーザーとして
+    context "as an authorized user" do
+      let(:user) { FactoryBot.create(:user) }
+      let(:project) {FactoryBot.create(:project, owner: user)}
+      
+      #プロジェクトを更新できること
+      it "updates a project" do
+        project_params = FactoryBot.attributes_for(:project, name: "New Project Name")
+        sign_in user
+        patch :update, params: { id: project.id, project: project_params }
+        expect(project.reload.name).to eq "New Project Name"
+      end
+    end
+
+    #認可されていないユーザーとして
+    context "as an unauthorized user" do
+      let(:user) { FactoryBot.create(:user) }
+      other_user = FactoryBot.create(:user)
+      let(:project) { FactoryBot.create(:project,
+        owner: other_user,
+        name: "Same Old Name") }
+
+      #プロジェクトを更新できないこと
+      it "does not update the project" do
+        project_params = FactoryBot.attributes_for(:project,
+          name: "New Name")
+        sign_in user
+        patch :update, params: { id: project.id, project: project_params }
+        expect(project.reload.name).to eq "Same Old Name"
+      end
+
+      # ダッシュボードへリダイレクトすること 
+      it "redirects to the dashboard" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in user
+        patch :update, params: { id: project.id, project: project_params }
+        expect(response).to redirect_to root_path
       end
     end
   end
